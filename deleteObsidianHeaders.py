2311a983b4ee5d9f2b1be2a2949f93c3f3ace352
Sarah Simionescu <97263859+sarahsimionescu@users.noreply.github.com>
2023-01-18 09:00:14 -0500

    Add delete obsidian headers
    This will recognize the first # header and remove it from the file. This was useful when Obsidian had a major update and the file names were automatically placed a the top of each file as a # header, and the manually added headers were now redundant.

---
+++ deleteObsidianHeaders.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+VAULT_PATH="insert path here"
+
+
+for root, subFolders, files in os.walk(VAULT_PATH):
+    for file in files:
+        file_path = os.path.join(root, file)
+        if file.endswith('.md'):
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+            counter = 0
+            with open(file_path,'w') as f:
+                for line in lines:
+                    if line.startswith("# ") and counter == 0:    
+                       print("Deleted " ,line ," from ", file)
+                       counter = 1
+                    else:
+                        f.write(line)
